4525e7912106f9f86801481e24cd46a3c2ca2f19
Robiul Islam Jubaer <113488476+jubaer013@users.noreply.github.com>
2023-05-05 11:14:38 +0600

    Add files via upload

---
+++ BEE1075.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+  int N;
+  cin >> N;
+  cout << "2" << endl;
+  for(int i=2; i<1000; i=i+N)
+  {
+      cout << i << endl;
+  }
+    return 0;
+}
